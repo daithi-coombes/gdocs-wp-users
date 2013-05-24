807f22b66b338d180ecfc5088812507df26faee6
daithi-coombes <webeire@gmail.com>
2013-05-24 14:30:12 +0100

    init commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package gdocs-wp-users
+ */
+/*
+  Plugin Name: GDocs Wordpress Users
+  Plugin URI: https://github.com/daithi-coombes/gdocs-wp-users
+  Description: Allows editing of user profiles on a wordpress database using google docs spreadsheet
+  Version: 0.1
+  Author: Daithi Coombes
+  Author URI: http://david-coombes.com
+ */
+
