48a4f7decac183b127ad465b2ab5a676cd8c301e
daithi-coombes <webeire@gmail.com>
2013-05-25 02:18:38 +0100

    update_user method

--- class-gas-wp-users.php
+++ class-gas-wp-users.php
@@ -13,4 +13,23 @@ class GAS_WP_Users{
 		$methods['gas_wp.update_user'] = array(&$this, 'update_user');
 		return $methods;
 	}
+
+	public function update_user( $params ){
+
+		global $wp_xmlrpc_server;
+
+		//vars
+		$username = $params[1];
+		$password = $params[2];
+		$args = $params[3];
+
+		//login user
+		if(!$wp_xmlrpc_server->login($username, $password))
+			return $wp_xmlrpc_server->error;
+		
+		//update user
+		$res = wp_update_user($args);
+		if(is_wp_error($res))
+			return $wp_xmlrpc_server->error('503', $res->get_error_message());
+	}
 }
--- tests/class-gas-wp-usersTest.php
+++ tests/class-gas-wp-usersTest.php
@@ -14,6 +14,12 @@ class GAS_WP_UsersTest extends PHPUnit_Framework_TestCase{
 		$this->obj = new GAS_WP_Users();
 	}
 
+	function tearDown(){
+		parent::tearDown();
+		$params = array('ID' => 1, 'first_name' => '', 'last_name' => '');
+		wp_update_user($params);
+	}
+
 	function test_xml_update_user(){
 		global $wp_xmlrpc_server;
 
@@ -36,14 +42,24 @@ class GAS_WP_UsersTest extends PHPUnit_Framework_TestCase{
 		$this->assertEquals('update_user', $method[1]);
 	}
 
-	function test_update_user(){
+	function test_update_user_login_success(){
 
-		//taken from parameter
-		$params = array(1,'admin','password', array(ID => 1, 'first_name' => 'foo', 'last_name' => 'bar'));
-
-		wp_update_user( $params [3] );
+		//update success
+		$params = array(1,'admin','password', array('ID' => 1, 'first_name' => 'foo', 'last_name' => 'bar'));
+		$res = $this->obj->update_user($params);
 		$user = get_userdata(1);
+		$this->assertEquals($user->user_firstname, 'foo', "gas_wp.update_user update success failed");
+
+		//login fail
+		$params[1] = 'unkown_user';
+		$res = $this->obj->update_user($params);
+		$this->assertTrue($res instanceof IXR_Error, "gas_wp.update_user login fail failed");
+
+		//update fail
+		$params[1] = 'admin';
+		$params[3]['ID'] = '4567';
+		$res = $this->obj->update_user($params);
+		print_r($res);
 		
-		$this->assertEquals('foo', $user->user_firstname);
 	}
 }
